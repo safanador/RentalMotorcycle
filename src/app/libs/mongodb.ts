178506dcb8f060d87867e91696dc9f7e455a8359
Sergio Afanador <sergioafanador102@gmail.com>
2024-04-20 14:08:00 -0500

    2.0

--- src/app/libs/mongodb.ts
+++ src/app/libs/mongodb.ts
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
-const MONGO_URL = process.env.MONGO_URI! as string
+const MONGODB_URI = process.env.MONGODB_URI! as string
 
 export const connectMongoDB = async () => {
     try {
-        await mongoose.connect(MONGO_URL);
+        await mongoose.connect(MONGODB_URI);
         console.log("Successfully conected to MongoDB rentbike database")
 
     } catch (error) {

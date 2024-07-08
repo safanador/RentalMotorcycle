60b63f1ebd21c635dde2138cd9c6d8fd94772edc
Sergio Afanador <sergioafanador102@gmail.com>
2024-07-08 00:43:23 -0500

    nav bar updates

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -3,29 +3,23 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import React, { Suspense } from "react";
-import {
-    Avatar,
-    AvatarFallback,
-    AvatarImage,
-  } from "@/components/ui/avatar"
-  
-import { Button } from "../ui/button";
+import { PiMotorcycleBold } from "react-icons/pi";
+import { FaPersonHiking } from "react-icons/fa6";
+import { RxAvatar } from "react-icons/rx";
+import { RxQuestionMarkCircled } from "react-icons/rx";
+import { BsInfoCircle } from "react-icons/bs";
+import { LiaCookieBiteSolid } from "react-icons/lia";
+
+
 
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuGroup,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuPortal,
-    DropdownMenuSeparator,
-    DropdownMenuShortcut,
-    DropdownMenuSub,
-    DropdownMenuSubContent,
-    DropdownMenuSubTrigger,
-    DropdownMenuTrigger,
-  } from "@/components/ui/dropdown-menu"
-import { DeleteCookie } from "@/app/services/deleteCookies";
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
 import { Menu } from "lucide-react";
 import SearchInput from "./SearchInput";
 
@@ -36,95 +30,93 @@ const links = [
     //{name:"Corporativo", href:"/corporate"},
 ]; 
 
+const linksSecondNav = [
+    {name:"Tours", href:"/tours", icon:<FaPersonHiking size={28} color="white"/>},
+    {name:"Motos rental", href:"/rental", icon:<PiMotorcycleBold size={28} color="white"/>},
+    
+    //{name:"Corporativo", href:"/corporate"},
+]; 
+
 function NavBar(){
     const pathname = usePathname()
-    const router = useRouter();
-
     return (
-        <header className="border-b "> {/*sticky top-0 */}
-        <div className="flex items-center justify-between px-5 p-2 shadow-sm border-b-[1px] h-[63px] ">
-            <Link href="/">
-                <h1 className="text-2xl md:text-4xl font-bold">L<span className="text-primary">OGO</span></h1>
-            </Link>
-            <Suspense>
-                <SearchInput/>
-            </Suspense>
-            <div className="flex items-center gap-5">
-                <nav className="hidden gap-12 lg:flex lg:items-center 2xl:ml-16">
-                {links.map((link, idx)=>(
-                 <div key={idx}>
-                    {pathname === link.href ? (
-                        <Link className="text-lg font-semibold text-primary" href={link.href}>
-                            {link.name}
-                        </Link>
-                    ):(
-                        <Link href={link.href} className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-primary">
-                            {link.name}
-                        </Link>
-                    )}
-                 </div>   
-                ))}
-                </nav>
-                <nav className="flex items-center">
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <button  className="outline-none bg-transparent text-gray-800 hover:bg-transparent hover:text-gray-800 sm:hidden">
-                            <Menu/>
-                        </button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent className="w-56">
-                        <DropdownMenuLabel>Menu</DropdownMenuLabel>
-                            <DropdownMenuSeparator />
-                            <DropdownMenuGroup>
-                            {links.map((link, idx)=>(
-                                <div key={idx}>
-                                    {pathname === link.href ? (
-                                    <DropdownMenuItem>
-                                        <Link className="text-xs font-semibold text-primary" href={link.href}>
-                                            {link.name}
-                                        </Link>
-                                    </DropdownMenuItem>
-                                ):(
-                                    <DropdownMenuItem>
-                                        <Link href={link.href} className="text-xs font-semibold transition duration-100 hover:text-primary">
-                                            {link.name}
-                                        </Link>
-                                    </DropdownMenuItem>
-                                )}
-                            </div>   
-                            ))}
-                            </DropdownMenuGroup>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-                </nav>
-                {/* 
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <button  className="outline-none bg-transparent text-gray-800 hover:bg-transparent hover:text-gray-800">
-                            <div className="flex items-center justify-center">
-                                <Avatar>
-                                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                                    <AvatarFallback>CN</AvatarFallback>
-                                </Avatar>
-                            </div>
-                        </button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent className="w-56">
-                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                            <DropdownMenuSeparator />
-                            <DropdownMenuGroup>
-                                <DropdownMenuItem>
-                                    Profile
-                                </DropdownMenuItem>
-                            </DropdownMenuGroup>
-                            <DropdownMenuItem onClick={handleLogout}>
-                                Log out
-                            </DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
+        <header className="border-b bg-blue-900 "> {/*sticky top-0 */}
+            <div className="flex items-center justify-between px-5 p-2  h-[60px] ">
+                <Link href="/">
+                    <h1 className="text-xl md:text-4xl font-bold text-white">Touring.com</h1>
+                </Link>
+                {/*
+                <Suspense>
+                    <SearchInput/>
+                </Suspense>
                 */}
+                <div className="flex items-center gap-5">
+                    <nav className="hidden gap-12 lg:flex lg:items-center 2xl:ml-16">
+                    {links.map((link, idx)=>(
+                    <div key={idx}>
+                        {pathname === link.href ? (
+                            <Link className="text-lg font-semibold text-primary" href={link.href}>
+                                {link.name}
+                            </Link>
+                        ):(
+                            <Link href={link.href} className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-primary">
+                                {link.name}
+                            </Link>
+                        )}
+                    </div>   
+                    ))}
+                    </nav>
+                    <nav className="flex items-center">
+                        <Link href="/login">    
+                            <RxAvatar color="white" className="mr-4 md:hidden" size={28} />
+                        </Link>
+                        
+                        <Sheet>
+                            <SheetTrigger className="outline-none bg-transparent text-gray-800 hover:bg-transparent hover:text-gray-800 md:hidden"><Menu color="white"/></SheetTrigger>
+                            <SheetContent>
+                                <SheetHeader>
+                                <SheetTitle className="mb-4">Más información</SheetTitle>
+                                <SheetDescription>
+                                    <div className="flex items-center mb-2">
+                                        <RxQuestionMarkCircled size={20} className="mr-2"/>
+                                        <p>Contact Customer Service</p>
+                                    </div>
+                                    <div className="flex items-center mb-2">
+                                        <BsInfoCircle size={19} className="mr-2"/>
+                                        <p>About Touring.com</p>
+                                    </div>
+                                    <div className="flex items-center mb-2">
+                                    <LiaCookieBiteSolid
+                                    size={20} className="mr-2"/>
+                                        <p>Privacy & cookies</p>
+                                    </div>
+                                </SheetDescription>
+                                </SheetHeader>
+                            </SheetContent>
+                        </Sheet>
+                    </nav>
+                </div>
             </div>
-        </div>
+            <div className="flex shadow-sm border-b-[1px] h-[60px]">
+                <nav className="flex">
+                    {linksSecondNav.map((link, idx)=>(
+                        <div key={idx}>
+                            {pathname === link.href ? (
+                                    <Link className="flex border p-3 border-white rounded-full items-center justify-center bg-blue-800 " href={link.href}>
+                                        {link.icon}
+                                        <p className="text-white text-sm font-medium ml-2">{link.name}</p>
+                                    </Link>
+                                ):(
+                                    <Link className="flex p-3  items-center justify-center hover:rounded-full hover:bg-blue-800 " href={link.href}>
+                                        {link.icon}
+                                        <p className="text-white text-sm font-medium ml-2">{link.name}</p>
+                                    </Link>
+                                )
+                            }
+                        </div>   
+                    ))}
+                </nav>     
+            </div>
         </header>
     )
 }
